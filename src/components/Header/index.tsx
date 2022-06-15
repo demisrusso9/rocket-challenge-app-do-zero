80e0dd22966be578c3dd920ff6c632795cbd287b
Demis Russo <demis.silva@compasso.com.br>
2022-06-15 11:53:10 -0300

    feat: header component
    Add styled Header component. Add on _app.tsx to appear on every page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,14 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <Link href="/">
+          <img src="/icons/logo.svg" alt="logo" />
+        </Link>
+      </div>
+    </div>
+  );
 }
